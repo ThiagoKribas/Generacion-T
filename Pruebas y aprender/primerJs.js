75144ea618d7f5f3fbf325e0893bb8dff9cd7148
ThiagoKribas <thiagokribas@gmail.com>
2022-10-04 17:00:38 -0300

    Creados archivos de JS, se incluyo bootstrap y se avanzo en diversos proyectos (pancho, starwars)

---
+++ Pruebas y aprender/primerJs.js
@@ -0,0 +1,27 @@
+/* alert("bienvenidos");
+let nombre = prompt('como te llamas?');
+alert('Hola: ' + nombre);
+let edad = prompt('Y tu edad?');
+console.log(edad);
+alert('Hola ' + nombre + ' de ' + edad + ' años');
+let texto = prompt('Ingresa un texto corto: ');
+console.log(texto.length);
+alert('viviste un total de ' + edad*365 + ' Dias, al cumplir los 18');
+let num1 = prompt('Ingresa dos valores a sumar porfavor: ');
+let num2 = prompt();
+console.log(Number(num1) + Number(num2)); 
+let edadmax = prompt('Hasta cuanto queres vivir?')
+let snack = prompt('Cual es tu snack favorito?')
+let preciosnack = prompt('Y cuanto salen?')
+let snackdiarios = prompt('cuantos ' + snack + ' comes por dia?')
+let edadres = edadmax - edad
+console.log(edadres)
+let snackedad = snackdiarios * edadres
+console.log(snackedad)
+alert('Necesitarás ' + snackedad + ' snacks para que te alcancen hasta los ' + edadmax + ' años.')
+alert('Esto significa que vas a gastar ' + (Number(preciosnack) * Number(snackedad)) + '$ en snacks!!!!') */
+
+let dias = prompt('Cuantos dias te vas de vacaciones?')
+let presupuestomax = prompt('Ingresa tu presupuesto maximo para las vacaciones:')
+let comidatotal = prompt('Cuantas veces vas a comer?')
+alert('Podés gastar ' + (Number(presupuestomax)/Number(comidatotal)) + ' en cada comida para que te alcance la plata durante los ' + dias + ' días de viaje')
